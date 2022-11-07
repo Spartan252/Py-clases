06332932ca8e0b88dea7755a19baf1f93e764839
Spartan252 <santiagoeli.ag@gmail.com>
2022-11-07 07:56:57 -0600

    -_-

--- Prácticas/Minesweeper.py
+++ Prácticas/Minesweeper.py
@@ -1,15 +1,54 @@
 
 import random
 
-fil = 0
-col = 0
+def GenerarMinas(tablero_v):
+    minas = 0 
+    while(minas < 100):
+        posición = random.randint(0, 479)
+        fil = posición // 30
+        col = posición % 30
 
-minas = 99
-while(minas != 0):
-    n = random.randint(0, 479)
+        if tablero_v[fil][col] != 9:
+            tablero_v[fil][col] = 9
+            minas += 1
+            return tablero_v
 
-fil = n//30
-col = n%30
 
-if([fil][col] == 0):
+def MostrarTablero(tablero_v, tablero_e):
+    print("  ", end= " ")
+    for i in range(fila):
+        print(format(i+1, "2d"), end= " ")
+    print()
+
+    for i in range(fila):
+        print(format(i+1, "2d"), end= " ")
+    print()
+    for j in range(columna):
+            print(tablero[i][j], end= " ")
+    else:
+            print(tablero_v)
+
+
+def InicializarTablero(tablero_e, tablero_v):
+    for i in range(fila):
+        filas = [0] * 30
+        tablero_v.append(filas)
+        filas = [0] * 30
+        tablero_e.append(filas)
+
+
+columna = int(input("Introduce el número de columnas: "))
+fila=int(input("Introduce el número de filas: "))
+archm = open("mapa.txt", "w")
+arche = open("estado.txt", "w")
+
+tablero=[]
+for i in range(fila):
+    for j in range(columna):
+        print([i+1,j+1], end="")
+        archm.write(str([i+1,j+1]) +" ")
+    print()
+    archm.write("\n")
+archm.close()
+
 
