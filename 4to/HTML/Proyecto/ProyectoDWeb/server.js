e884d3ff846f3455ebff041328ac582f4f214998
Spartan252 <santiagoeli.ag@gmail.com>
2024-05-20 08:27:49 -0600

    nose

--- 4to/HTML/Proyecto/ProyectoDWeb/server.js
+++ 4to/HTML/Proyecto/ProyectoDWeb/server.js
@@ -13,5 +13,9 @@ app.use('/views', express.static('views'));
 
 
 app.listen(port, () => {
+<<<<<<< HEAD
     console.log("Aplicación corriendo en puerto " + port);
+=======
+    console.log("Aplicación de ejemplo corriendo en puerto " + port);
+>>>>>>> 9b165b63264c8eae6d3be81585ba9bff1b14ad2a
 });
